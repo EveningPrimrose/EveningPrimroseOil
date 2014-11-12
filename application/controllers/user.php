eb935f9276ae55b815fdca511ddd3685773a668e
huangyupeng <1025942449@qq.com>
2014-11-12 20:22:46 +0800

    version 0.03

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -1,4 +1,6 @@
 <?php
+
+    header("Content-Type:text/html; charset=UTF-8");
     class User extends CI_Controller{
 
     
@@ -16,7 +18,7 @@
     		$pwd=$getpwd->result();
         
     		if (empty($pwd))
-    			echo "No this user!<br>";
+    			echo "No this user!<br>";//可增错误页面
     	
             
       		else if ($pwd[0]->password==$password)
@@ -29,11 +31,49 @@
     		}
     		else
     		{
-    			echo "Password is wrong!<br>";
+    			echo "Password is wrong!<br>";//可增页面
     		}
     	}
 
+        
+        public function signupcheck(){
+            $signupname= $this->input->post('signname');
+            $signupemail= $this->input->post('signemail');
+            $signuppwd= $this->input->post('signpwd');
+            $signuppwd2= $this->input->post('signpwd2');
+            echo $signupname;
+            if ($signuppwd!=$signuppwd2)
+            {
+                echo "密码错误！<br>";//可增页面
+            }
+            else
+            {
+                $this->load->database();
+                $sql="select * from users where username = '".$signupname."';";
+                $res=$this->db->query($sql);
+                $exist=$res->result();
+                if (empty($exist)){
 
+                    $sql="insert into users (username,password,email) values ('".$signupname."','".$signuppwd."','".$signupemail."');";
+                    $res=$this->db->query($sql);
+
+                    if ($res)
+                    echo "注册成功<br>";
+                }
+                else
+                {
+                    echo "用户已存在！<br>";
+                } 
+            }
+            
+        }
+
+
+        public function signup(){
+            $this->load->library('session');
+            $data['username']=$this->session->userdata('username');
+            $this->load->view('signup.html',$data);
+        }
 
         public function home()
         {
